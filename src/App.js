65938b33b8f0ff4d44a3f8d612bfd03915dadbfd
mattshardman <mattshardman@gmail.com>
2019-10-22 15:15:14 +0100

    add back app.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./App.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
