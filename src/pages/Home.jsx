3ba42126b8a9223ec339b4bf2189398b1f0dbc99
mattshardman <mattshardman@gmail.com>
2019-10-22 12:01:55 +0100

    call init endpoint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import withLogin from '../auth/withLogin';
+import { api } from '../lib/api';
 
 function Home() {
+
+    const init = async () => {
+        try {
+            const res = await api.init();
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    useEffect(() => {
+        init();
+    }, []);
     return (
         <div>
             Home
@@ -8,4 +24,4 @@ function Home() {
     );
 }
 
-export default Home;
+export default withLogin(Home);
