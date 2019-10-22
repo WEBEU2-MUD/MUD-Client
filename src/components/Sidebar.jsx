1affd215585e922bf73daa54e3335a5d4f39886c
mattshardman <mattshardman@gmail.com>
2019-10-22 15:52:25 +0100

    create sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import styled from "styled-components";
+
+const SideContainer = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  width: 300px;
+  height: 100vh;
+  border-right: 1px solid #c5c5c5;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+
+  button {
+    position: absolute;
+    box-sizing: border-box;
+    bottom: 20px;
+    height: 40px;
+    width: calc(100% - 40px);
+    background: none;
+    border: 1px solid #c5c5c5;
+    border-radius: 4px;
+    color: #c5c5c5;
+    cursor: pointer;
+  }
+`;
+
+const User = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Avatar = styled.div`
+  border-radius: 50%;
+  background: #c5c5c5;
+  height: 40px;
+  width: 40px;
+  margin-right: 10px;
+  color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Players = styled.div`
+  padding: 20px 0px;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  
+  div {
+    height: 400px;
+    overflow: scroll;
+  }
+
+  h3 {
+    margin: 5px 0;
+  }
+
+  p {
+    padding: 0 10px;
+    margin: 5px 0;
+  }
+`;
+
+function SideBar({ name, location, logOut }) {
+  return (
+    <SideContainer>
+      <User>
+        <Avatar>{name[0] && name[0].toUpperCase()}</Avatar>
+        <h3>{name[0] && `${name[0].toUpperCase()}${name.slice(1)}`}</h3>
+      </User>
+      <Players>
+        <h3>Players in this room</h3>
+        <div>
+            {location.players.map(player => {
+            return <p key={player}>{player}</p>;
+            })}
+        </div>
+      </Players>
+      <button onClick={logOut}>Log Out</button>
+    </SideContainer>
+  );
+}
+
+export default SideBar;
