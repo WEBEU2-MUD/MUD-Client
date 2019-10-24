50aa63f236578d0470f498ec4db27fc5f4420288
mattshardman <mattshardman@gmail.com>
2019-10-24 19:16:11 +0100

    change layout

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useCallback } from "react";
 import styled from "styled-components";
 import { api } from "../lib/api";
-import Room from "./Room";
+import Roomio from "./Roomio";
 
 const GameContainer = styled.div`
   box-sizing: border-box;
   padding-top: 50px;
+  padding-bottom: 50px;
   width: 100%;
   height: 100vh;
   display: flex;
@@ -23,40 +24,6 @@ const Info = styled.div`
     width: 800px;
 `;
 
-const Controls = styled.div`
-  display: grid;
-  grid-template-columns: 50px 50px 50px;
-  grid-template-rows: 50px 50px;
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
-  padding-bottom: 20px;
-
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid #c5c5c5;
-    border-radius: 4px;
-  }
-
-  div:first-child {
-    border: none;
-  }
-
-  div:nth-child(3) {
-    border: none;
-  }
-
-  button {
-    width: 100%;
-    height: 100%;
-    background: none;
-    border: none;
-    color: #c5c5c5;
-    cursor: pointer;
-  }
-`;
-
 const Loader = styled.div`
     color: #c5c5c5;
     position: fixed;
@@ -70,6 +37,7 @@ function Game({ location, setLocation }) {
         setMoving(direction);
         try {
           const data = await api.move(direction);
+          console.log(data)
           setMoving(false);
           setLocation(data);
         } catch (err) {
@@ -83,36 +51,11 @@ function Game({ location, setLocation }) {
   return (
     <GameContainer>
        { moving && <Loader>Moving</Loader> }
-       {!!location.error_msg && !moving && <Loader className="error">{location.error_msg}</Loader>}
       <Info>
         <h1>You are in: {location.title}</h1>
         <p>{location.description}</p>
       </Info>
-      <Room location={location} moving={moving}/>
-      <Controls direction={moving}>
-        <div></div>
-        <div>
-          <button disabled={moving} onClick={() => clickHandler("n")}>
-            North
-          </button>
-        </div>
-        <div></div>
-        <div>
-          <button disabled={moving} onClick={() => clickHandler("w")}>
-            West
-          </button>
-        </div>
-        <div>
-          <button disabled={moving} onClick={() => clickHandler("s")}>
-            South
-          </button>
-        </div>
-        <div>
-          <button disabled={moving} onClick={() => clickHandler("e")}>
-            East
-          </button>
-        </div>
-      </Controls>
+      <Roomio location={location} moving={moving} clickHandler={clickHandler} />
     </GameContainer>
   );
 }
